4a3468770284b787f90d0ac56e12110e6571ddbf
MasterUNG <phrombutr@gmail.com>
2018-03-31 15:16:21 +0700

    Initial commit

---
+++ app/src/main/java/masterung/androidthai/in/th/mycalculatebasic/MainFragment.java
@@ -0,0 +1,53 @@
+package masterung.androidthai.in.th.mycalculatebasic;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Show Rate
+        TextView textView = getView().findViewById(R.id.txtShowRate);
+        String urlAPI = "https://api.fixer.io/latest?symbols=THB&base=USD";
+        try {
+
+            GetCurrentMoneyRate getCurrentMoneyRate = new GetCurrentMoneyRate(getActivity());
+            getCurrentMoneyRate.execute(urlAPI);
+            String jsonString = getCurrentMoneyRate.get();
+            Log.d("31MachV1", "json0 ==> " + jsonString);
+
+            jsonString = jsonString.substring(0, jsonString.length() - 2);
+            Log.d("31MachV1", "json1 ==> " + jsonString);
+
+            jsonString = jsonString.substring(jsonString.length() - 5, jsonString.length());
+            Log.d("31MachV1", "json2 ==> " + jsonString);
+
+            textView.setText("1 --> " + jsonString);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }   // Main Method
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
